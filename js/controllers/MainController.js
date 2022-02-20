9c3e36d406393ebc537bad30c5bdbffa2cac3716
SeungHwan-Lee <sh981013s@gmail.com>
2022-02-20 21:02:44 +0900

    docs: README 요구사항 작성

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,9 +1,7 @@
-import FormView from '../views/FormView.js'
-
-const tag = '[MainController]'
+const tag = "[MainController]";
 
 export default {
   init() {
-    FormView.setup(document.querySelector('form'))
+    console.log(tag, "init()");
   },
-}
+};
